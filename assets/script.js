53e7f4670df26f19e4378a1bfb9c82a900865949
amiekisiak <117371691+amiekisiak@users.noreply.github.com>
2023-02-01 21:21:04 +0000

    added changes to readme

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+
+// for the html file:
+// 1. use bootsrap
+// 2. create a map and make it clickable
+// 3. create popup
+// 4. integrate API country facts
+// 5. integrate API developer EDAMAM 
+// 6. create structure for pop up (what should be displayed inside)
+// country name, how many recipes we want to be shown
+// 7. create cool logo for header
+// 8. create dropdown menu with saved recipes
+// 9. Use the MealDB API 
