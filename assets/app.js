06867beb32a424ce47555e48d62c694880996ddf
ArturK <vaselisk999@gmail.com>
2023-02-02 23:33:58 +0000

    Added countries list, changed onRegionClick  click method, fixed styles, and html

--- assets/app.js
+++ assets/app.js
@@ -38,6 +38,8 @@ $(document).ready(function () {
   </nav>'
   
   );
+
+
 });
 
 //-----------------------------------------------
@@ -81,18 +83,32 @@ $('#search').on('click', function() {
 //   var buttonName = $(event.target).text();
 //           //function to display favorite recipe (buttonName)
 //       });
-
-//Modal//
-
-$('#exampleModal').on('show.bs.modal', function (event) {
-  var button = $(event.relatedTarget) // Button that triggered the modal
-  var recipient = button.data('whatever') // Extract info from data-* attributes
-  // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-  // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-  var modal = $(this)
-  modal.find('.modal-title').text('New message to ' + recipient)
-  modal.find('.modal-body input').val(recipient)
-})
  
+//class SearchApiClass
+//ToDo find new name for class
+function SearchApiClass(country){
+  //country code
+  this.country = country;
+
+  //class method searche by click on country 
+  this.searcheByCountry = function (){
+    var self = this;
+
+    //get country data
+    getCountryData(this.country, function(data){
+      console.log(data);
+      console.log(self.country);
+      $("#exampleModal").modal('show');
+      $("#exampleModalLabel").text(self.country);
+    });
+
+  }
+
+
+  //class method 
+  this.searcheByInput = function(){
+
+  }
+}
 
   
