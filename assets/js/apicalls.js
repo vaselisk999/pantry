e768d0c90cf36a097758054546b62e17ddd13775
ArturK <vaselisk999@gmail.com>
2023-02-06 13:21:42 +0000

    added search input padding, extra spaces, search input field validation

--- assets/js/apicalls.js
+++ assets/js/apicalls.js
@@ -1,7 +1,6 @@
 // Description: This file contains all the API calls for the application
 
 // Function to get country data from restcountries API
-
 async function getCountryData(country, callback) {
   try {
     const response = await fetch(
@@ -13,15 +12,13 @@ async function getCountryData(country, callback) {
     console.log(error);
   }
 }
+
 // Function to get recipe data from spoonacular API
 async function getRecipeData(country, callback) {
   try {
     const response = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&app_id=${APP_ID}&app_key=${APP_KEY}&q=${country}`);
     const data = await response.json();
     callback(data);
-    
-    
-    
   } catch (error) {
     console.log(error);
   }
