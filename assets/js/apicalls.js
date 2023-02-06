ecb867c4d520e46fb0f3b105b2f8df3d0ab12d30
ArturK <vaselisk999@gmail.com>
2023-02-06 11:57:04 +0000

    apicalls edamam api

--- assets/js/apicalls.js
+++ assets/js/apicalls.js
@@ -14,20 +14,15 @@ async function getCountryData(country, callback) {
   }
 }
 // Function to get recipe data from spoonacular API
-function getRecipeData(searchQuery) {
-  const options = {
-    method: "GET",
-    headers: {
-      "X-RapidAPI-Key": X_RapidAPI_Key,
-      "X-RapidAPI-Host": X_RapidAPI_Host,
-    },
-  };
-
-  return fetch(
-    `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/complexSearch?query=${searchQuery}`,
-    options
-  )
-    .then((response) => response.json())
-    .then((response) => console.log(response))
-    .catch((err) => console.error(err));
+async function getRecipeData(country, callback) {
+  try {
+    const response = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&app_id=${APP_ID}&app_key=${APP_KEY}&q=${country}`);
+    const data = await response.json();
+    callback(data);
+    
+    
+    
+  } catch (error) {
+    console.log(error);
+  }
 }
