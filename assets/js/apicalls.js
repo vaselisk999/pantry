88c512178b126b8c3c56ba3e4bbe35268fbaf46b
amiekisiak <117371691+amiekisiak@users.noreply.github.com>
2023-02-06 11:53:53 +0000

    new changes

--- assets/js/apicalls.js
+++ assets/js/apicalls.js
@@ -4,26 +4,26 @@
 // Function to get country data from restcountries API
 
 async function getCountryData(country, callback) {
-    try {
-      const response = await fetch(`https://restcountries.com/v3.1/name/${country}`);
-      const data = await response.json();
-      callback(data);
-    } catch (error) {
-      console.log(error);
-    }
+  try {
+    const response = await fetch(`https://restcountries.com/v3.1/name/${country}`);
+    const data = await response.json();
+    callback(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// // function to get recipes from EDAMAM API
+async function getRecipeData(country, callback) {
+  try {
+    const response = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&app_id=${APP_ID}&app_key=${APP_KEY}&q=${country}`);
+    const data = await response.json();
+    callback(data);
+    
+    
+    
+  } catch (error) {
+    console.log(error);
   }
   
-
-// function to get recipes from EDAMAM API
-async function getRecipeData(country, callback) {
-    try {
-      const response = await fetch(`https://api.edamam.com/search?q=${country}&app_id=${APP_ID}&app_key=${APP_KEY}`);
-      const data = await response.json();
-      callback(data);
-      
-      
-    } catch (error) {
-      console.log(error);
-    }
-    
-  }
+}
